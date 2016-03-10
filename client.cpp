e2d0ee0144355959db231132c13e2e8f611bb96c
DaniilStepanov <stepanov0995@mail.ru>
2016-03-10 16:02:38 +0300

    Add serialization for SerStr

--- client.cpp
+++ client.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include "ipc/Client.h"
 #include "DB.hpp"
+#include "serStr.pb.h"
 
 
 struct serializableStruct {
@@ -16,14 +17,23 @@ struct serializableStruct {
     serializableStruct(int k1, const std::string& l1) : k(k1), l(l1) {}
 
     static leveldb_daemon::serializer::Buffer serialize(const serializableStruct &){
-        std::shared_ptr<char> l(new char);
-        memcpy(l.get(), "toJSON", 6);
-        return {l, 6};
+        example::SerStr ser;
+        ser.set_l("lol");
+        ser.set_k(0);
+        std::string serial;
+        ser.SerializeToString(&serial);
+        std::shared_ptr<char> sp(new char[10],std::default_delete<char[]>());
+        for(int i=0;i<serial.size();++i){
+            sp.get()[i]=serial[i];
+        }
+        return {sp, serial.size()};
     }
 
     static serializableStruct deserialize(const leveldb_daemon::serializer::Buffer& ser, const std::string& context) {
-        auto&& s = serializableStruct(3, ser.array.get());
-        return s;
+        example::SerStr des;
+        std::string st=ser.array.get();
+        des.ParseFromString(st);
+        return serializableStruct((int)des.k(),des.l());
     }
 
     static serializableStruct notFound() {
@@ -34,16 +44,15 @@ struct serializableStruct {
 
 
 int main() {
-    serializableStruct s = {0, "lol"};
-    leveldb_daemon::db::write<serializableStruct>("key", s);
-    s.l = "asa";
-    leveldb_daemon::db::write<serializableStruct>("key1", s);
+
 
     std::string ctx;
+    example::SerStr ser;
+    serializableStruct s = {0, "lol"};
+    leveldb_daemon::db::write<serializableStruct>("key", s);
     auto&& res = leveldb_daemon::db::read<serializableStruct, std::string>("key", ctx);
     for (auto&& it: res) {
         std::cout << it.l << std::endl;
     }
-
     return 0;
 }
